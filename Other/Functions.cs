e5eb4f0c81fc2780c7cc3dbff0e7506ace5e46a0
mightymop <totzkotz@gmail.com>
2025-04-04 23:20:45 +0200

    improve check

--- Other/Functions.cs
+++ Other/Functions.cs
@@ -7,15 +7,19 @@ namespace fahrtenbuch_service.Other
     public class Functions
     {
         private static ILog log = LogManager.GetLogger(typeof(Functions));
-       
+
         public static bool IsWebPageAvailable(string url)
+        {
+            return IsWebPageAvailableInternal(url, allowRetry: true);
+        }
+
+        private static bool IsWebPageAvailableInternal(string url, bool allowRetry)
         {
             try
             {
                 HttpWebRequest request = (HttpWebRequest)WebRequest.Create(url);
                 request.Method = "GET";
-                request.Timeout = 5000;
-                request.AllowAutoRedirect = false;
+                request.Timeout = 10000;
                 request.AllowAutoRedirect = true;
                 request.ServerCertificateValidationCallback += (sender, certificate, chain, sslPolicyErrors) => true;
 
@@ -27,6 +31,15 @@ namespace fahrtenbuch_service.Other
             catch (Exception e)
             {
                 log.Error("IsWebPageAvailable() url: " + url, e);
+
+                // Retry once with HTTP if HTTPS failed and retry is allowed
+                if (allowRetry && url.StartsWith("https://", StringComparison.OrdinalIgnoreCase))
+                {
+                    string httpUrl = "http://" + url.Substring("https://".Length);
+                    log.Info("Retrying with HTTP: " + httpUrl);
+                    return IsWebPageAvailableInternal(httpUrl, allowRetry: false);
+                }
+
                 return false;
             }
         }
